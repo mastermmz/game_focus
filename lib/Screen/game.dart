6b139f9dba380caf65c609194f1c7909209a43d8
mastermmz <mm2041772@gmail.com>
2022-08-26 01:55:08 +0430

    Ability to save points in the classic mood

--- lib/Screen/game.dart
+++ lib/Screen/game.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:game_focus/models/models.dart';
+import '../main.dart';
 
 class GameScreen extends StatefulWidget {
   const GameScreen({Key? key , required this.pointsRequired, required this.gameMode}) : super(key: key);
@@ -13,18 +15,22 @@ class GameScreen extends StatefulWidget {
 class _GameScreenState extends State<GameScreen> {
 
   //int pointsRequired = widget.pointsRequired;
+  final TextEditingController _controllerName = TextEditingController();
   int numberOfItem = 0;
   int numberOfScore = 0;
   bool endGame = false;
+  bool saveData = false;
   DateTime? timeRunGame;
   Duration? chekGameTime;
+
+  bool hasbininsert = false;
+
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    randomColor();
     timeRunGame = DateTime.now();
-    print(timeRunGame);
+    randomColor();
   }
 
   void randomColor (){
@@ -35,6 +41,12 @@ class _GameScreenState extends State<GameScreen> {
     }
     numberOfItem = rnd;
   }
+
+  void _SaveNumeData({required Duration? timer ,required String userName}){
+    DataTimeRecord dataTimeRecord = DataTimeRecord(name: userName, time: timer.toString());
+    objectBox.saveDataRecord(dataTimeRecord);
+  }
+
 
   void loadTeameGame(){
     chekGameTime =  DateTime.now().difference(timeRunGame!);
@@ -50,10 +62,12 @@ class _GameScreenState extends State<GameScreen> {
         centerTitle: true,
         title:  Text("Score: $numberOfScore / ${widget.pointsRequired}"),
       ),
-      body: _buildBoody()
+      body: _buildBoody(),
     );
-  }
 
+
+
+  }
 
   Widget _buildBoody(){
     if(endGame == false){
@@ -83,17 +97,8 @@ class _GameScreenState extends State<GameScreen> {
                       }
                     });
                   },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: numberOfItem == index? Color(0xff006D77) : Color(0xff83C5BE),
-                      borderRadius: BorderRadius.all(Radius.circular(100)) ,
-                    ),
-                    height: double.infinity / 5,
-                    width: double.infinity / 4,
-                    child: Text(
-                      '',
-                      style: Theme.of(context).textTheme.headline5,
-                    ),
+                  child: CircleAvatar(
+                    backgroundColor: numberOfItem == index? Color(0xff006D77) : Color(0xff83C5BE),
                   ),
                 );
               }),
@@ -104,18 +109,80 @@ class _GameScreenState extends State<GameScreen> {
       return _endGamebuildBoody();
     }
   }
-
+//color: numberOfItem == index? Color(0xff006D77) : Color(0xff83C5BE),
 
   Widget _endGamebuildBoody(){
     if(widget.gameMode == "classic"){
+      if(saveData){
+        return Container(
+          alignment: Alignment.center,
+          child: const Text("Data has been saved" , style: TextStyle(
+            fontSize: 35.0,
+            fontWeight: FontWeight.w500
+          )),
+        );
+      }
       return Container(
         child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("classic Mode End Game" , style: TextStyle(fontSize: 40.0)),
-                SizedBox(height: 20.0),
+                const Text("classic Mode End Game" , style: TextStyle(fontSize: 40.0)),
+                const SizedBox(height: 20.0),
                 Text("$chekGameTime", style: TextStyle(fontSize: 36.0)),
+                Container(
+                    margin: const EdgeInsets.only(top: 35.0 , left: 15.0, right: 15.0),
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: const BoxDecoration(
+                      boxShadow: [BoxShadow(
+                        color: Color(0xff303438),
+                        blurRadius: 5,
+                        offset: Offset(3, 10), // Shadow position
+                      ),],
+                      borderRadius: BorderRadius.all(Radius.circular(20)) ,
+                      color: Color(0xff4895ef),
+                    ),
+                    child:Center(child: TextField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      cursorColor: Colors.black,
+                      controller: _controllerName,
+                      //style: Theme.of(context).textTheme.headline5,
+                      decoration: const InputDecoration(
+                          labelText: "Doto Description",
+                          labelStyle: TextStyle(color:  Colors.black)
+                      ),
+                    )
+                    )
+                ),
+                GestureDetector(
+                  onTap: (){
+                    if(_controllerName.text != ""){
+                      _SaveNumeData(timer: chekGameTime ,userName: _controllerName.text );
+                      setState(() {
+                        saveData = true;
+                      });
+                    }else{
+                      print("else");
+                    }
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 35.0 , left: 15.0, right: 15.0),
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: const BoxDecoration(
+                      boxShadow: [BoxShadow(
+                        color: Color(0xff303438),
+                        blurRadius: 5,
+                        offset: Offset(3, 10), // Shadow position
+                      ),],
+                      borderRadius: BorderRadius.all(Radius.circular(20)) ,
+                      color: Color(0xff4895ef),
+                    ),
+                    child: const Text("SAVE" , style: TextStyle(
+                        fontSize: 35.0
+                    )),
+                  ),
+                )
               ],)),
       );
     }else{
@@ -124,9 +191,9 @@ class _GameScreenState extends State<GameScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("unlimited Mode End Game" , style: TextStyle(fontSize: 40.0)),
-                SizedBox(height: 20.0),
-                Text("$chekGameTime", style: TextStyle(fontSize: 36.0)),
+                const Text("unlimited Mode End Game" , style: TextStyle(fontSize: 40.0)),
+                const SizedBox(height: 20.0),
+                Text("$chekGameTime", style: const TextStyle(fontSize: 36.0)),
               ],)),
       );
     }
